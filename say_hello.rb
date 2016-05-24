1f6f09a79b164cb58b5f020d49cbe62e79a325fa
Alberto Gomez <aeg407@nyu.edu>
2016-05-24 15:38:05 -0400

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,9 @@
-# Build your say_hello method here
+def say_hello
+  prints "Hello,#{{name}}"
+
+def say_hello(name = "Ruby Programmer")
+  prints "Hello, #{name}!"
+end
+
+say_hello
+say_hello("Gabriela")
