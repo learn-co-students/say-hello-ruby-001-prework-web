e128694c4403ee0396534eeabe580db155933b17
Michael Wiggins <michael.wiggins@me.com>
2016-08-12 17:06:22 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,7 @@
+#this method defines an argument that takes a default value "Ruby Programmer".
+#if you called it without an argument, it would print "Ruby Programmer"
+#if you called it with an argument, it would update and use your argument
+# say_hello("John")
 def say_hello (name = "Ruby Programmer")
   puts "Hello #{name}!"
 end
